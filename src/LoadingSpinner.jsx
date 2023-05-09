0fedeb83ca38027f9ac87f662a20679d972d98b1
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2023-05-09 10:44:19 +0000

    Create LoadingSpinner component for loading

---
+++ src/LoadingSpinner.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./index.css";
+
+export default function LoadingSpinner() {
+  return (
+    <div className="spinner-container">
+      <div className="loading-spinner">
+      </div>
+    </div>
+  );
+}
