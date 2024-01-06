d65113a0f9f90d564cb682676c6cfad7f1e0218b
Rithik2211 <rithikpranao22@gmail.com>
2024-01-06 23:56:35 +0530

    week6

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,7 @@ function App() {
 
   async function getTokenBalance() {
     const config = {
-      apiKey: '<-- COPY-PASTE YOUR ALCHEMY API KEY HERE -->',
+      apiKey: 't6l5ShkHmI97nfbVwsW6FBnk0KVSO001',
       network: Network.ETH_MAINNET,
     };
 
